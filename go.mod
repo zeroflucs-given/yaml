8adb84a6612a77faaa87f9c67496cddfc9782780
Julian Peterson <julian@zeroflucs.io>
2026-02-18 14:47:35 +1000

    Revert "feat: add file parameter to UnmarshalWithOrigin"
    This reverts commit 4f1c3d02ddd4d2b2f37cb2b1fcd63d82e7a6cac6.

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/oasdiff/yaml
 
 go 1.22.5
 
-require github.com/oasdiff/yaml3 v0.0.0-20260217201013-8a620da6102a
+require github.com/oasdiff/yaml3 v0.0.0-20250309153720-d2182401db90
--- yaml.go
+++ yaml.go
@@ -16,7 +16,7 @@ import (
 	"reflect"
 	"strconv"
 
-	yaml "github.com/oasdiff/yaml3"
+	"github.com/oasdiff/yaml3"
 )
 
 // Marshal the object into JSON then converts JSON to YAML and returns the
@@ -44,14 +44,14 @@ type YAMLOpt func(*yaml.Decoder) *yaml.Decoder
 // Unmarshal converts YAML to JSON then uses JSON to unmarshal into an object,
 // optionally configuring the behavior of the JSON unmarshal.
 func Unmarshal(y []byte, o interface{}, opts ...JSONOpt) error {
-	return UnmarshalWithOrigin(y, o, false, "", opts...)
+	return UnmarshalWithOrigin(y, o, false, opts...)
 }
 
 // UnmarshalWithOrigin is like Unmarshal but if withOrigin is true, it will
 // include the origin information in the output.
-func UnmarshalWithOrigin(y []byte, o interface{}, withOrigin bool, file string, opts ...JSONOpt) error {
+func UnmarshalWithOrigin(y []byte, o interface{}, withOrigin bool, opts ...JSONOpt) error {
 	dec := yaml.NewDecoder(bytes.NewReader(y))
-	dec.Origin(withOrigin, file)
+	dec.Origin(withOrigin)
 	return unmarshal(dec, o, opts)
 }
 
